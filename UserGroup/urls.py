eb638d4c78338823eac4c06465595497564b981f
翁彦彬 <diandianhanbin@gmail.com>
2017-03-30 14:49:05 +0800

    登录首页迁移完毕

--- UserGroup/urls.py
+++ UserGroup/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='usergroupindex'),
 	url(r'^user/', views.user, name='user'),
 	url(r'^usercenter/', views.usercenter, name='usercenter'),
 	url(r'^operationlog/', views.operation_log, name='operationlog'),
